7e408a50875272c44b6ab08505f8e72b8876e5e0
´LuiKashi´ <luivelandiabaez@crackthecode.la>
2025-09-12 20:17:00 -0500

    refactor:ultimos cambios en inicio

--- assets/js/nosotras.js
+++ assets/js/nosotras.js
@@ -1,47 +1,37 @@
-console.log("Hola");
+console.log ("Hola");
 document.querySelector(".experiencia").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  // Guardamos todos los campos en un array
   const campos = [
     document.getElementById("Nombres"),
     document.getElementById("Correo"),
     document.getElementById("Experiencia"),
   ];
 
-  let vacios = false; // Bandera para saber si falta algo
+  let vacios = false;
 
-  // Recorremos cada campo con un bucle
 
   for (let i = 0; i < campos.length; i++) {
     if (!campos[i] || campos[i].value.trim() === "") {
       alert("Por favor llenar este campo:" + campos[i].id);
       campos[i].style.border = "2px solid red";
       vacios = true;
-      break; // rompe el bucle apenas encuentra uno vacío
+      break;
     } else {
       campos[i].style.border = "2px solid green";
     }
   }
-  /*  for (let i = 0; i < campos.length; i++) {
-  if (!campos[i] || campos[i].value.trim() === "") {
-    campos[i].style.border = "2px solid red";
-    vacios = true;
-  } 
-} */
 
   if (vacios) {
     return;
   } else {
     console.log("Experiencia enviada: ", campos[2].value);
-
-    //document.querySelector(".experiencia").reset();
-
     const button = document.querySelector(".experiencia button");
     button.textContent = "Enviando ....";
     setTimeout(() => {
       button.textContent = "Enviar";
     }, 2000);
+  }
 
     const mensaje = document.createElement("p");
     mensaje.textContent = "✅ ¡Gracias por compartir tu experiencia!";
